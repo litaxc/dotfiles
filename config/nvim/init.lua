2ab5c62ea419512532783f80edaaa1b1c8cad854
lita <fireice47@gmail.com>
2024-10-07 08:23:21 +0800

    adj ipy key

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -6,7 +6,7 @@ vim.o.expandtab = true
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 vim.o.cursorline = true
-vim.o.scrolloff = 100
+vim.o.scrolloff = 5
 
 vim.o.clipboard = 'unnamed'
 
--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -184,12 +184,15 @@ autocmd('User', {
                     opts = opts or { silent = true }
                     vim.keymap.set(mode, l, r, opts)
                 end
-                map("n", "<space>ii", ":MoltenInterrupt<CR>")
-                map("n", "<space>oh", ":MoltenHideOutput<CR>")
-                map("n", "<space>oo", ":noautocmd MoltenEnterOutput<CR>")
-                map('n', '<space>x', function() nbv.run_cell() end)
-                map('n', '<space>X', function() nbv.run_and_move() end)
+                map("n", "<space>i", ":MoltenInterrupt<CR>")
+                map("n", "<space>h", ":MoltenHideOutput<CR>")
+                map("n", "<space>o", ":noautocmd MoltenEnterOutput<CR>")
+                map('n', '<space>X', function() nbv.run_cell() end)
+                map('n', '<space>x', function() nbv.run_and_move() end)
                 map("v", "<space>x", function() vim.cmd "MoltenEvaluateVisual" end)
+                map('n', '<space>a', function() nbv.add_cell_above() end)
+                map('n', '<space>b', function() nbv.add_cell_below() end)
+                map('n', '<space>-', function() nbv.split_cell() end)
                 map('n', ']x', function() nbv.move_cell 'd' end)
                 map('n', '[x', function() nbv.move_cell 'u' end)
             end
